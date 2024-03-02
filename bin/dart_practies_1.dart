aa5a70285997a12a3ccc8e334996ce6fff739496
arifbiswas <mxgangstar310@gmail.com>
2024-03-02 23:03:00 +0600

    how assgin veriable

--- bin/dart_practies_1.dart
+++ bin/dart_practies_1.dart
@@ -1,3 +1,41 @@
+// void main(){
+//   /* print("Execute number 5 ");
+//   print("Execute number 6 ");
+//    */
+//   // print("Execute number 7 ");
+//   print("Execute number 8 ");
+//
+//   print("bidare polashe polabo polbo mon");
+//
+// }
+
 void main(){
-  print(5 );
+
+
+  // print("bidare polashe polabo polbo mon");
+// var isActive = true;
+  // const isArif = true;
+  // const nameList = ["arif","sakib","rakib","abul","kabul"];
+  // var products = {
+  //   'name' : 'Xoimi 11 pro max',
+  //   "prise" : 20000,
+  //   "location" : "205/Road amtoili bangan, Bangladesh",
+  //   "used" : "none"
+  // };
+
+  var a = 50 ;
+  var b =10 ;
+  var c = 5;
+
+
+  print(++a);
+  print(a++);
+  print(a++);
+  print(a++);
+  print(a++);
+  print(a++);
+
+
+
+
 }
