af00d6248bdabe18b8c25740bbd55ff895062bcb
arifbiswas <mxgangstar310@gmail.com>
2024-03-03 14:17:28 +0600

    for in loop

---
+++ bin/forInLoo.dart
@@ -0,0 +1,6 @@
+void main(){
+  const myList = [50,54,54,6485,45,54.58];
+  for(var i in myList){
+    print(i);
+  }
+}
---
+++ bin/forLoop.dart
@@ -0,0 +1,6 @@
+void main(){
+  var num = 10;
+  for(int i = 0 ; i <= num; i++){
+    print(i);
+  }
+}
